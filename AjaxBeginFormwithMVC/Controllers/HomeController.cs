59d1599c26508f578cb01285d5b0c4995ef28af4
Naimur Rahman <naimproff@github.com>
2022-05-31 23:52:17 +0600

    Add project files.

---
+++ AjaxBeginFormwithMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using AjaxBeginFormwithMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxBeginFormwithMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult EmployeeMaster()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EmployeeMaster(EmpModel obj)
+        {
+            ViewBag.Records = "Name : " + obj.Name + " City:  " + obj.City + " Addreess: " + obj.Address;
+            return PartialView("EmployeeMaster");
+        }
+    }
+}
---
+++ AjaxBeginFormwithMVC/Models/EmpModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjaxBeginFormwithMVC.Models
+{
+   public class EmpModel
+    {
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
